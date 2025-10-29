942def137a06ac66619cc52bd4ebda5d3a111de2
neolao <contact@neolao.com>
2025-10-29 22:32:01 +0100

    Update startup

--- Graphics/0_Startup/Script.lua
+++ Graphics/0_Startup/Script.lua
@@ -12,7 +12,10 @@
 
 local loadingAnimeType = 0
 
-local optkAngle = 0
+local logoAngle = 0
+local bgWidth = 1920
+local bgX = 0
+local bgY = 0
 
 function clearIn(player)
 end
@@ -22,6 +25,7 @@ end
 
 function init()
     func:AddGraph("Background.png")
+    func:AddGraph("OpenTaiko.png")
 
     if loadingAnimeType == 0 then
         func:AddGraph("logo.png")
@@ -30,16 +34,26 @@ function init()
 end
 
 function update()
+    bgX = bgX - (bgWidth * deltaTime / 50.0)
+    if bgX < -bgWidth then
+        bgX = 0
+    end
+
     if loadingAnimeType == 0 then
-        optkAngle = optkAngle - (60 * deltaTime)
+        logoAngle = logoAngle - (60 * deltaTime)
     elseif loadingAnimeType == 1 then
     end
 end
 
 function draw()
-    func:DrawGraph(0, 0, "Background.png")
+    for i = 0, 3 do
+        func:DrawGraph(bgX + i * bgWidth, bgY, "Background.png")
+    end
+
+    func:DrawGraph(1080, 930, "OpenTaiko.png")
+
     if loadingAnimeType == 0 then
-        func:SetRotation(optkAngle, "logo.png")
+        func:SetRotation(logoAngle, "logo.png")
         func:DrawGraph(1720, 880, "logo.png")
     elseif loadingAnimeType == 1 then
     end
